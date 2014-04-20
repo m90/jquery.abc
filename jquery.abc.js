67f2ecd0f09fd0ce97d81dec31363fb7e32d9397
m90 <frederik.ring@gmail.com>
2014-04-20 21:37:33 +0200

    save coding style from being so 2013

--- jquery.abc.js
+++ jquery.abc.js
@@ -11,16 +11,17 @@
 
 	function _cpaToString(cpa, width, charSize, blocks, colors){
 
-		var reducedPixelArray = [];
+		var
+		reducedPixelArray = []
+		, blockArray = []
+		, letters = [];
 
 		//get rid of alpha and reduce RGB to a single average value
 		for (var b = 0, c = cpa.length; b < c; b += 4){
-			reducedPixelArray.push(Math.max(cpa[b], cpa[b+1], cpa[b+2]));
+			reducedPixelArray.push(Math.max(cpa[b], cpa[b + 1], cpa[b + 2]));
 		}
 
 		//this will contain one luminance value for each letter-block
-		var blockArray = [];
-
 		for (var h = 0; h < blocks.y; h++){ //move along y at block level
 
 			for (var i = 0; i < blocks.x; i++){ //move along x at block level
@@ -31,7 +32,7 @@
 				for (var j = 0; j < charSize.y; j++){ //move along y at pixel level
 
 					for (var k = 0; k < charSize.x; k++){ //move along x at pixel level
-										
+
 						var currentOffset = baseOffset + (j * width + k);
 							pixelsInBlock.push(reducedPixelArray[currentOffset]);
 
@@ -40,121 +41,120 @@
 					}
 
 				//average all pixels in the current block and push into blockArray
-				blockArray.push(~~(pixelsInBlock.reduce(function(a,b){return a + b;}) / (charSize.x * charSize.y)));
+				blockArray.push(parseInt(pixelsInBlock.reduce(function(a,b){return a + b;}) / (charSize.x * charSize.y), 10));
 
 			}
 
 		}
 
-					
-		var letters = [];
-
 		for (var l = 0, m = blockArray.length; l < m; l++){
 
-			letters.push($.grep(colors, function(el, i){
-					
+			letters.push($.grep(colors, function(el){
 				return (blockArray[0] <= el.top && blockArray[0] >= el.bottom);
-				
 			})[0].letter);
 
 			blockArray.shift();
 
 			if ((l + 1) % blocks.x === 0){
-				
+
 				letters.push('\n'); //append line break
-			
+
 			}
 
 		}
 
-		//Array.join() is much faster than String += in IEs
 		return letters.join('');
-	
+
 	}
 
 	var methods = {
 
 		init : function(options){
 
-			options || (options = {}); //options passed?
-
-			/* OPTIONS */
 			var settings = $.extend({
-
 				letters : 'XMI/-.', //letters used, dark to light
-				invert : false,
-				fps: 25 //invert the letters if light text is used on a dark background
-
-			}, options);
+				invert : false, //invert the letters if light text is used on a dark background
+				fps: 25
+			}, options || {});
 
 			return this.each(function(){
 
 				var letters = settings.invert ? settings.letters.split('').reverse() : settings.letters.split(''),
 				colors = [],
 				dimensions = {
-					
-					width: $(this).width(),
-					height: $(this).height()
-					
+					width: $(this).width()
+					, height: $(this).height()
 				};
 
 				for (var a = 0; a < letters.length; a++){
-
 					colors.push({
-
-						top: 255 / letters.length * (a + 1),
-						bottom: 255 / letters.length * a,
-						letter: letters[a]
-							
+						top: 255 / letters.length * (a + 1)
+						, bottom: 255 / letters.length * a
+						, letter: letters[a]
 					});
-
-				};
+				}
 
 				//GET SIZE OF A SINGLE MONOSPACED CHARACTER
-				var dummy = $('<span>').text('a').css({'font-family' : 'monospace', 'visibility' : 'hidden'}).appendTo($(this).parent());
-					
-					var charSize = {
-						x : dummy.width(),
-						y : dummy.height()
-					};
+				var dummy = $('<span>').text('a').css({
+					'font-family' : 'monospace'
+					, 'visibility' : 'hidden'
+				}).appendTo($(this).parent());
+
+				var charSize = {
+					x : dummy.width()
+					, y : dummy.height()
+				};
 
 				dummy.remove();
 
 				var blocks = {
-
-					x : ~~(dimensions.width / charSize.x),
-					y : ~~(dimensions.height / charSize.y)
-
+					x : parseInt(dimensions.width / charSize.x, 10)
+					, y : parseInt(dimensions.height / charSize.y, 10)
 				};
 
-				$(this).wrap($('<span>', {'class' : 'abc-wrapper', 'width': dimensions.width, 'height': dimensions.height}).css({'display' : $(this).css('display'),  'overflow' : 'hidden'})).hide();
+				$(this).wrap($('<span>', {
+					'class' : 'abc-wrapper'
+					, 'width': dimensions.width
+					, 'height': dimensions.height
+				}).css({
+					'display' : $(this).css('display')
+					, 'overflow' : 'hidden'
+				})).hide();
 
 				if (this.nodeName === 'IMG'){ //IMAGE
 
 					var
-					$img = $(this),
-					pic = $('<canvas>').attr(dimensions),
-					picCtx = pic[0].getContext('2d');
+					$img = $(this)
+					, pic = $('<canvas>').attr(dimensions)
+					, picCtx = pic[0].getContext('2d')
+					, pixelArray
+					, art;
 
 					picCtx.drawImage($img[0], 0, 0, dimensions.width, dimensions.height);
 
-					var pixelArray = picCtx.getImageData(0, 0, dimensions.width, dimensions.height).data;
+					pixelArray = picCtx.getImageData(0, 0, dimensions.width, dimensions.height).data;
 
-					var art = '<pre style="line-height:' + charSize.y + 'px;">' + _cpaToString(pixelArray, dimensions.width, charSize, blocks, colors) + '</pre>';
+					art = [
+						'<pre style="line-height:'
+						, charSize.y, 'px;">'
+						, _cpaToString(pixelArray, dimensions.width, charSize, blocks, colors)
+						, '</pre>'
+					].join('');
+
 					$img.after(art);
-
 
 				} else if (this.nodeName === 'VIDEO'){ //VIDEO
 
 					var
-					$vid = $(this),
-					vid = $('<canvas>').attr(dimensions),
-					vidCtx = vid[0].getContext('2d');
+					$vid = $(this)
+					, vid = $('<canvas>').attr(dimensions)
+					, vidCtx = vid[0].getContext('2d')
+					, videoInterval;
 
-					$vid.after('<pre style="line-height:' + charSize.y + 'px;"></pre>');
+					$vid.after(['<pre style="line-height:', charSize.y, 'px;"></pre>'].join(''));
 
-					var videoInterval = setInterval(function(){
-						
+					videoInterval = setInterval(function(){
+
 						vidCtx.clearRect(0, 0, dimensions.width, dimensions.height);
 						vidCtx.drawImage($vid[0], 0, 0, dimensions.width, dimensions.height);
 
@@ -163,12 +163,10 @@
 
 					}, 1000 / settings.fps);
 
-					$(this).data('abcInterval', videoInterval);
+					$vid.data('abcInterval', videoInterval);
 
 				} else {
-
 					$.error('.abc() can only be applied to image and video elements!');
-
 				}
 
 			});
@@ -187,25 +185,19 @@
 
 			});
 
-		} 
+		}
 
 	};
 
 
-	$.fn.abc = function(method) {
+	$.fn.abc = function(method){
 
 		if (methods[method]) {
-
 			return methods[method].apply(this, [].slice.call(arguments, 1));
-
 		} else if ( typeof method === 'object' || ! method ) {
-
 			return methods.init.apply( this, arguments );
-
 		} else {
-
 			$.error( 'Method ' +  method + ' does not exist on jQuery.abc' );
-
 		}
 
 	};
